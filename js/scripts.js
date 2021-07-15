42ef3b2625e791af3d0b8924a8520a691a92efc5
shoshanaucla <shoshanaucla@gmail.com>
2021-07-15 10:27:30 -0700

    Added navtoggle menu function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello and thank you for visiting!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
